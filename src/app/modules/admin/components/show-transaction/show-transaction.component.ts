44b42489e98035b40a83049b29664ff23ddc270f
munavvar78 <91778581+munavvar78@users.noreply.github.com>
2024-06-25 23:51:05 +0530

    changes

--- src/app/modules/admin/components/show-transaction/show-transaction.component.ts
+++ src/app/modules/admin/components/show-transaction/show-transaction.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Expense, Incomes, Transection } from '../../../../common/models/expenses.model';
 import { ApiServiceService } from '../../../../common/services/apiService.service';
 import { MaterialModule } from '../../../../common/matrial/matrial.module';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-show-transaction',
@@ -15,7 +16,13 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class ShowTransactionComponent implements OnInit {
   transactionData: any[] = [];
   filteredData: any[] = [];
+  paginatedData: any[] = [];
   filterForm: FormGroup;
+  pageSize = 10;
+  pageIndex = 0;
+  totalItems = 0;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private apiService: ApiServiceService, private fb: FormBuilder) {
     this.filterForm = this.fb.group({
@@ -23,7 +30,7 @@ export class ShowTransactionComponent implements OnInit {
         start: [''],
         end: ['']
       }),
-      searchQuery: [' ']
+      searchQuery: ['']
     });
   }
 
@@ -50,7 +57,8 @@ export class ShowTransactionComponent implements OnInit {
             ];
 
             this.transactionData = combinedData.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-            this.filteredData = this.transactionData;
+            this.totalItems = this.transactionData.length;
+            this.applyFilter();
           },
           error: err => {
             console.error(err);
@@ -67,7 +75,7 @@ export class ShowTransactionComponent implements OnInit {
     const { dateRange, searchQuery } = this.filterForm.value;
     const startDate = dateRange.start ? new Date(dateRange.start).getTime() : null;
     const endDate = dateRange.end ? new Date(dateRange.end).getTime() : null;
-    const query = searchQuery.toLowerCase().trim();
+    const query = searchQuery.toLowerCase();
 
     this.filteredData = this.transactionData.filter(transaction => {
       const transactionDate = new Date(transaction.date).getTime();
@@ -78,6 +86,21 @@ export class ShowTransactionComponent implements OnInit {
 
       return dateMatch && searchMatch;
     });
+
+    this.totalItems = this.filteredData.length;
+    this.paginateData();
+  }
+
+  paginateData() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedData = this.filteredData.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.paginateData();
   }
 
   clearDateRange() {
