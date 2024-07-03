6e567266093094a0f5f437625cd9c8c21dc53897
Munavvar Popatiya <tatva.dotnet.munavvarpopatiya@outlook.com>
2024-07-03 09:56:34 +0530

    changes

--- src/app/modules/admin/components/category/add-category/add-category.component.ts
+++ src/app/modules/admin/components/category/add-category/add-category.component.ts
@@ -3,11 +3,12 @@ import { MaterialModule } from '../../../../../common/matrial/matrial.module';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Category } from '../../../../../common/models/expenses.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-category',
   standalone: true,
-  imports: [MaterialModule,ReactiveFormsModule],
+  imports: [MaterialModule,ReactiveFormsModule,CommonModule],
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css'
 })
